c883bd50b7d9b08ae25cf984f29fc502fc1731f4
Gabukuro <scatamburlo.gabriel@gmail.com>
2019-11-12 21:44:17 -0300

    alterando router de actuators

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin/tempSensors', tempSensorsRouterAdmin);
 app.use('/admin/devices', devicesRouterAdmin);
-app.use('/actuator', actuatorsRouter);
+app.use('/actuators', actuatorsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
